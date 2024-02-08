9e0f6a475d90662b53ca572562ce29098a8dc4f0
sinachi ajuzie <Ajuziesinachi@gmail.com>
2024-02-08 20:08:26 +0100

    Done: Adding the router contract for tracking contracts.

--- hardhat/contracts/AppContract.sol
+++ hardhat/contracts/AppContract.sol
@@ -2,19 +2,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./IRouter.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title AppNFT
+/// @title AppContract
 /// @author NatX
 /// @notice Contract that enables Developers/organization to create applications NFTs
 /// @dev This is a template contract that Devs can deploy that will enable users mint applications and application packs
-contract AppNFT is ERC1155, Ownable {
+contract AppContract is ERC1155, Ownable {
     using Counters for Counters.Counter; // OpenZeppelin Counter
     Counters.Counter private _idCount; // Counter for id of pack NFTs created
 
     uint256 public appPrice;
-    address public routerAddress;
+    IAppRouter public router;
 
     struct Pack {
         uint256 price;
@@ -27,27 +28,20 @@ contract AppNFT is ERC1155, Ownable {
     constructor (string memory uri_, uint256 _price, address _routerAddress) ERC1155(uri_) Ownable() {
         appPrice = _price;
         _idCount.increment();
-        routerAddress = _routerAddress;
+        router = IAppRouter(_routerAddress);
+        router.registerApp(address(this));
     }
 
-    function createPack(uint256 quantity, uint256 price) external onlyOwner {
+    function createPack(uint256 quantity, uint256 price, address caller) external {
+        require(caller == owner(), "You are not permitted to call this function");
         Packs[_idCount.current()].maxQuantity = quantity;
         Packs[_idCount.current()].price = price;
 
         _idCount.increment();
     }
 
-    // write modifier to allow only owner or main contract to mint token
-    function mintPack(address to, uint256 id) external payable onlyRouter {
-        require(Packs[id].maxQuantity > Packs[id].amountMinted, "Pack unavailable");
+    function buyPack(address to, uint256 id) external payable {
         require(msg.value == Packs[id].price, "Pay the correct amount");
-
         _mint(to, id, 1, "");
     }
-
-    modifier onlyRouter() {
-        require(msg.sender == routerAddress || msg.sender == owner(), "You are not allowed to call this function");
-        _;
-    }
 }
-
